324bed4af60da5cb6080118f360d17e0fcd8c1d6
undecode <undecode0@hotmail.com>
2023-02-09 01:20:23 -0600

    CRUD Completed

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE,   OPTIONS");
-  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE,   OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     require_once '../clasess/database/DBConnection.php';
     require_once '../clasess/dao/ProductDAO.php';
@@ -24,56 +24,49 @@
           
           break;
         case 'POST':
-          
-          // parse_str(file_get_contents("php://input"), $data);
-          // $name = $put_vars["name"];
-          // $name = $data["name"];
-          // $description = $data["description"];
+          if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['price']) && isset($_FILES['image'])) { 
 
-          // if(isset($name)){
-          //   echo json_encode('success');
-          // }
-          echo json_encode(); 
+            $image_name = $_FILES['image']['name'];
+            $image_tmp = $_FILES['image']['tmp_name'];
 
-          // if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['price']) && isset($_FILES['image'])) { 
+            if (is_uploaded_file($image_tmp)) {
+                $destination = "../assets/img/" . $image_name;
+                move_uploaded_file($image_tmp, $destination);
+            }
 
-          //   $image_name = $_FILES['image']['name'];
-          //   $image_tmp = $_FILES['image']['tmp_name'];
-
-          //   if (is_uploaded_file($image_tmp)) {
-          //       $destination = "../assets/img/" . $image_name;
-          //       move_uploaded_file($image_tmp, $destination);
-          //   }
-
-          //   $created = $productDao->createProduct($_POST['name'], $_POST['description'], $_POST['price'], $image_name);
+            $created = $productDao->createProduct($_POST['name'], $_POST['description'], $_POST['price'], $image_name);
             
-          //   $response = array('messsage' => (($created) ? 'Product created successfully' : 'Failed to create product'));
-
-          //   echo json_encode($response); 
-          // }
+            echo json_encode($created); 
+          }
 
           break;
         case 'PUT':
           
-          parse_str(file_get_contents("php://input"), $put_vars);
+          function getValue($property, $rawData) {
+            preg_match("/Content-Disposition: form-data; name=\"$property\"\r\n\r\n(.+)\r\n/", $rawData, $matches);
+            return $matches[1];
+          }
+
+          $rawData = file_get_contents("php://input");
           
-          if (isset($put_vars['id']) && isset($put_vars['name']) && isset($put_vars['description']) && isset($put_vars['price'])) {
-            $updated = $productDao->updateProduct($put_vars['id'], $put_vars['name'], $put_vars['description'], $put_vars['price']);
-            
-            $response = array('messsage' => (($updated) ? 'Product updated successfully' : 'Failed to update product'));
-            echo json_encode($response); 
+          $id = getValue("id", $rawData);
+          $name = getValue("name", $rawData);
+          $description = getValue("description", $rawData);
+          $price = getValue("price", $rawData);
+          
+          if(isset($id) && isset($name) && isset($description) && isset($price)){
+            $updated = $productDao->updateProduct($id, $name, $description, $price);
+
+            echo json_encode($updated);
           }
           
           break;
         case 'DELETE':
             
-          parse_str(file_get_contents("php://input"), $delete_vars);
-          
-          if (isset($delete_vars['id'])) {
-            $deleted = $productDao->deleteProduct($delete_vars['id']);
+          if (isset($_GET['id'])) {
+            $deleted = $productDao->deleteProduct($_GET['id']);
             
-            $response = array('messsage' => (($deleted) ? 'Product deleted successfully' : 'Failed to delete product'));
-            echo json_encode($response); 
+            echo json_encode($deleted);
           }
 
           break;
